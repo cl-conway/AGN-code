b264c8325575a629f2a2b123c544d3359255dae2
Nick Kinsey <NKK300@student.bham.ac.uk>
2016-10-14 15:12:00 +0100

    Historgram production

--- PG1302_Function_11-10-2016.py
+++ PG1302_Function_11-10-2016.py
@@ -31,7 +31,7 @@ import seaborn; seaborn.set()
 from datetime import datetime
 
 #Iteration function
-def Iteration_function(Data, number, Output_figs):
+def Iteration_function(Data, number, Output_figs, file_loc):
 
     #Create numpy arrays of the time and errors to pass to L-S model
     Times = Data[['MJD']].as_matrix()
@@ -39,14 +39,18 @@ def Iteration_function(Data, number, Output_figs):
     Mag_orgs = Data[['Mag']].as_matrix()
 
     #Define a new list to place the editted data into
-    New_list = []
     New_array = np.empty([290,1], dtype=float)
 
+    #The first element is always the original data set
     if number==0:
+
         #Build LSF model and generate file paths
         model_original = LombScargleFast().fit(Times, Mag_orgs, Errors)
-        file_path_org = 'C:/Users/User/Documents/University/Year 4/Project/Iteration_figures_11-10-2016/Light_Curve_org.jpg'
-        file_path_org2 = 'C:/Users/User/Documents/University/Year 4/Project/Iteration_figures_11-10-2016/LSF_org.jpg'
+        file_path_org = file_loc + '/Light_Curve_org.jpg'
+        file_path_org2 = file_loc + '/LSF_org.jpg'
+
+        #Build original model
+        periods_orig, power_orig = model_original.periodogram_auto(nyquist_factor=10)
 
         #Create Light Curve
         if Output_figs == 1:
@@ -57,13 +61,7 @@ def Iteration_function(Data, number, Output_figs):
             ax.invert_yaxis();
             fig.savefig(file_path_org, bbox_inches='tight')
 
-        #Build original model
-        periods_orig, power_orig = model_original.periodogram_auto(nyquist_factor=10)
-
-        print(Mag_orgs.shape)
-
-        #Plot the original LSF model
-        if Output_figs == 1:
+            #Plot the original LSF model
             fig, ax = plt.subplots()
             ax.plot(periods_orig, power_orig, color="r")
             ax.set(xlim=(0, 2000),
@@ -87,18 +85,14 @@ def Iteration_function(Data, number, Output_figs):
     for i in range(Data.Mag.count()):
         newvalue = random.gauss(Data['Mag'].iloc[[i]], Data['Magerr'].iloc[[i]])
         New_array[i,0] = newvalue
-        #New_list.append(float(newvalue))
-
-    #Create numpy array from the list, for L-S model input
-    print(New_array)
 
     #Build L-S model
     model = LombScargleFast().fit(Times, New_array, Errors)
 
     #Apply the LS mechanism to the data
     periods, power = model.periodogram_auto(nyquist_factor=10)
-    file_path = 'C:/Users/User/Documents/University/Year 4/Project/Iteration_figures_11-10-2016/LSF_' + str(number) + '.jpg'
-    file_path2 = 'C:/Users/User/Documents/University/Year 4/Project/Iteration_figures_11-10-2016/Light_Curve_' + str(number) + '.jpg'
+    file_path = file_loc + '/LSF_' + str(number) + '.jpg'
+    file_path2 = file_loc + '/Light_Curve_' + str(number) + '.jpg'
     iteration_title = 'PG1302-102 Light Curve with Red Noise iteration ' + str(number)
 
     #Plotting the Light Curve using seaborn style
@@ -126,12 +120,12 @@ def Iteration_function(Data, number, Output_figs):
         model.optimizer.period_range=(500, 2000)
         period = model.best_period
 
-    #print(period)
-
     return period
 
+    #End of Iteration_function
+
 #Calling function calls the iteration function
-def calling_function(Data, Iterations, Outputs):
+def calling_function(Data, Iterations, Outputs, Output_loc):
 
     #Define a period list, to store results
     Period_List = []
@@ -143,34 +137,43 @@ def calling_function(Data, Iterations, Outputs):
 
     #Perform the iterations
     for j in range(0, Iterations):
-        New_Period = Iteration_function(Data, j, Output_n)
+        New_Period = Iteration_function(Data, j, Output_n, Output_loc)
         Period_List.append(int(New_Period))
 
-    #Print the list
-    print(Period_List)
+    #Call the LS_hist function in order to create the Histogram
+    LS_hist(Period_List, Output_loc)
 
-    #Write the results to a text file
-    results_file = open("C:/Users/User/Documents/University/Year 4/Project/Iteration_figures_11-10-2016/Results.txt","w")
-    results_file.write("\n".join(map(lambda x: str(x), Period_List)) + "\n")
-    results_file.close()
+    #End of calling_function
 
-#Read in the Raw Data from the relevant location
-Raw_Data = pd.read_csv("http://nesssi.cacr.caltech.edu/DataRelease/upload/result_web_file5sP2FZ.csv", sep=',', header=0)
+def LS_hist(Results, file_loc):
+    print("Zero counts :", Results.count(0))
+    for j in range (Results.count(0)):
+        Results.remove(0)
+    plt.hist(Results, bins='auto')
+    plt.title("Histogram with 'auto' bins")
+    file_path_hist = file_loc + '\Hist.jpg'
+    plt.savefig(file_path_hist, bbox_inches='tight')
 
-#Set the number of iterations here
-No_of_Iterations = 4
+    #End of LS_hist function
 
-#Whether to output figures or not (Y/N; note for large no. of iterations put N)
-Output_figures = 'Y'
+def main():
+    #Read in the Raw Data from the relevant location
+    Raw_Data = pd.read_csv("http://nesssi.cacr.caltech.edu/DataRelease/upload/result_web_file5sP2FZ.csv", sep=',', header=0)
 
-#Calling function runs program
-calling_function(Raw_Data, No_of_Iterations, Output_figures)
+    #Set the number of iterations here
+    No_of_Iterations = 100
 
-"""
-It appears that the random number generation works and indeed that the whole
-process works. However, the L-S model, when presented with data that does not
-contain a clear period, seems to default with a very small little peak at
-a value of the period of 364.5 days. To overcome this, we could limit the data
-to above 1000 days. Same problem still exists, but has now changed to 2155.1
-this is not good.
-"""
+    #Whether to output Light curve and L-S figures or not (Y/N)
+    Output_figures = 'N'
+
+    #Output_location
+    Output_locations = 'C:/Users/User/Documents/University/Year 4/Project/Iteration_figures_11-10-2016'
+
+    #Calling function runs program
+    calling_function(Raw_Data, No_of_Iterations, Output_figures, Output_locations)
+
+    #End of main function
+
+main()
+
+#End of File
