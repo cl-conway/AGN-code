6b67510a2a4ea23ca07166ad7bf186bf168db784
Nick Kinsey <NKK300@student.bham.ac.uk>
2016-10-17 15:38:29 +0100

    Multiple Objects analysed, change objects by editting text file

--- PG1302_Function_11-10-2016.py
+++ PG1302_Function_11-10-2016.py
@@ -4,9 +4,12 @@ File for taking data for specific object, performs Gaussian distribution of
 magnitudes among the errors for each individual point. A Lomb-Scargle-Fast
 model is created for each iteration. Relevant graphs saved to a specific
 location and the best period for each LSF model is saved and printed.
+All variables to be changed in 'main' function. Rest of code will run
+appropriately.
 
 Inputs: Raw Data (e.g. for PG1302-102)
-http://nesssi.cacr.caltech.edu/DataRelease/upload/result_web_file5sP2FZ.csv
+C:/Users/User/Documents/University/Year 4/Project/Raw_Data_Inputs.txt
+[Internet connection Required to run]
 
 Exports to:
 C:/Users/User/Documents/University/Year 4/Project/Iteration_figures_11-10-2016
@@ -31,7 +34,7 @@ import seaborn; seaborn.set()
 from datetime import datetime
 
 #Iteration function
-def Iteration_function(Data, number, Output_figs, file_loc):
+def Iteration_function(Data, number, Output_figs, file_loc, Object):
 
     #Create numpy arrays of the time and errors to pass to L-S model
     Times = Data[['MJD']].as_matrix()
@@ -39,15 +42,15 @@ def Iteration_function(Data, number, Output_figs, file_loc):
     Mag_orgs = Data[['Mag']].as_matrix()
 
     #Define a new list to place the editted data into
-    New_array = np.empty([290,1], dtype=float)
+    New_array = np.empty([len(Data),1], dtype=float)
 
     #The first element is always the original data set
     if number==0:
 
         #Build LSF model and generate file paths
         model_original = LombScargleFast().fit(Times, Mag_orgs, Errors)
-        file_path_org = file_loc + '/Light_Curve_org.jpg'
-        file_path_org2 = file_loc + '/LSF_org.jpg'
+        file_path_org = file_loc + '/Light_Curve_org_' + Object + '.jpg'
+        file_path_org2 = file_loc + '/LSF_org_' + Object + '.jpg'
 
         #Build original model
         periods_orig, power_orig = model_original.periodogram_auto(nyquist_factor=10)
@@ -90,10 +93,10 @@ def Iteration_function(Data, number, Output_figs, file_loc):
     model = LombScargleFast().fit(Times, New_array, Errors)
 
     #Apply the LS mechanism to the data
-    periods, power = model.periodogram_auto(nyquist_factor=10)
-    file_path = file_loc + '/LSF_' + str(number) + '.jpg'
-    file_path2 = file_loc + '/Light_Curve_' + str(number) + '.jpg'
-    iteration_title = 'PG1302-102 Light Curve with Red Noise iteration ' + str(number)
+    periods, power = model.periodogram_auto(nyquist_factor=10, oversampling=50)
+    file_path = file_loc + '/LSF_' + Object + '_' + str(number) + '.jpg'
+    file_path2 = file_loc + '/Light_Curve_' + Object + '_' + str(number) + '.jpg'
+    iteration_title = Object + 'Light Curve with Red Noise iteration ' + str(number)
 
     #Plotting the Light Curve using seaborn style
     if Output_figs == 1:
@@ -110,7 +113,7 @@ def Iteration_function(Data, number, Output_figs, file_loc):
         ax.set(xlim=(0, 2000),
                 xlabel='Period (Days)',
                 ylabel='Lomb-Scargle Power',
-                title='LS Power against period plot');
+                title=Object + ' LS Power against period plot');
         fig.savefig(file_path, bbox_inches='tight')
 
     #Calculating the best period of the model, condition on quality of LSF model
@@ -125,7 +128,7 @@ def Iteration_function(Data, number, Output_figs, file_loc):
     #End of Iteration_function
 
 #Calling function calls the iteration function
-def calling_function(Data, Iterations, Outputs, Output_loc):
+def calling_function(Data, Iterations, Outputs, Output_loc, Object):
 
     #Define a period list, to store results
     Period_List = []
@@ -137,31 +140,48 @@ def calling_function(Data, Iterations, Outputs, Output_loc):
 
     #Perform the iterations
     for j in range(0, Iterations):
-        New_Period = Iteration_function(Data, j, Output_n, Output_loc)
+        New_Period = Iteration_function(Data, j, Output_n, Output_loc, Object)
         Period_List.append(int(New_Period))
 
     #Call the LS_hist function in order to create the Histogram
-    LS_hist(Period_List, Output_loc)
+    LS_hist(Period_List, Output_loc, Object)
 
     #End of calling_function
 
-def LS_hist(Results, file_loc):
+#Historgram function plots a histogram of the created data
+def LS_hist(Results, file_loc, Object):
+
+    #Print the number of zero counts within the sample
     print("Zero counts :", Results.count(0))
-    for j in range (Results.count(0)):
+
+    #Remove all the zero counts
+    for j in range(Results.count(0)):
         Results.remove(0)
+
+    #Plot the Historgram appropriately
+    fig, ax = plt.subplots()
     plt.hist(Results, bins='auto')
-    plt.title("Histogram with 'auto' bins")
-    file_path_hist = file_loc + '\Hist.jpg'
+    plt.title(Object + " Histogram with 'auto' bins")
+    file_path_hist = file_loc + '\Hist ' + Object + '.jpg'
     plt.savefig(file_path_hist, bbox_inches='tight')
 
     #End of LS_hist function
 
+#Main function to be called
 def main():
-    #Read in the Raw Data from the relevant location
-    Raw_Data = pd.read_csv("http://nesssi.cacr.caltech.edu/DataRelease/upload/result_web_file5sP2FZ.csv", sep=',', header=0)
+
+    #Define the location of the saved path
+    Data_location = 'C:/Users/User/Documents/University/Year 4/Project/Raw_Data_Inputs.txt'
+
+    #Read in the objects and URLs to be searched
+    Raw_Data_input = pd.read_table(Data_location, sep=',', header=0)
+
+    #save each column within the dataframe as a seperate numpy array
+    Objects = Raw_Data_input[['Name']].as_matrix()
+    URLs = Raw_Data_input[['URL']].as_matrix()
 
     #Set the number of iterations here
-    No_of_Iterations = 100
+    No_of_Iterations = 1000
 
     #Whether to output Light curve and L-S figures or not (Y/N)
     Output_figures = 'N'
@@ -169,8 +189,15 @@ def main():
     #Output_location
     Output_locations = 'C:/Users/User/Documents/University/Year 4/Project/Iteration_figures_11-10-2016'
 
-    #Calling function runs program
-    calling_function(Raw_Data, No_of_Iterations, Output_figures, Output_locations)
+    #Call the calling function for each object in the data
+    for k in range(0, len(Raw_Data_input)):
+        Object = Objects.item(k,0)
+        URL = URLs.item(k,0)
+        URL_editted = URL.strip()
+        Raw_Data = pd.read_csv(URL_editted, sep=',', header=0)
+
+        #Calling function runs program
+        calling_function(Raw_Data, No_of_Iterations, Output_figures, Output_locations, Object)
 
     #End of main function
 
