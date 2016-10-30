7520d671eede27f3372a666d02a6519f02f51a2d
Nick Kinsey <NKK300@student.bham.ac.uk>
2016-10-30 10:34:31 +0000

    Light Curve Plotting

--- PG1302_Function_11-10-2016.py
+++ PG1302_Function_11-10-2016.py
@@ -5,11 +5,20 @@ magnitudes among the errors for each individual point. A Lomb-Scargle-Fast
 model is created for each iteration. Relevant graphs saved to a specific
 location and the best period for each LSF model is saved and printed.
 All variables to be changed in 'main' function. Rest of code will run
-appropriately.
+appropriately. The function 'Granham_Candidate_Grab_Function' will grab, from
+the Graham_Candidates_names text file, the periodic candidates names and the
+Right Ascension and Declination values for these objects. The funtion will then
+input these into the CRTS database, locate the relevant URL for the data and
+save this URL to a the Raw_Data_Inputs text file. The authors advise running
+this function independantly from the rest of the program in order to reduce
+computational running time.
 
 Inputs: Raw Data (e.g. for PG1302-102)
 C:/Users/User/Documents/University/Year 4/Project/Raw_Data_Inputs.txt
-or C:/Users/Christopher/Documents/UNI/Year 4/Project/AGN-code/Raw_Data_Inputs.txt
+C:/Users/User/Documents/University/Year 4/Project/Graham_Candidates_names.txt
+or
+C:/Users/Christopher/Documents/UNI/Year 4/Project/AGN-code/Raw_Data_Inputs.txt
+C:/Users/Christopher/Documents/UNI/Year 4/Project/AGN-code/Graham_Candidates_names.txt
 [Internet connection Required to run]
 
 Exports to:
@@ -38,20 +47,21 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 #Iteration function
-def Iteration_function(Data, number, Output_figs, file_loc, Object):
+def Iteration_function(Data, number, Output_figs, file_loc, Object, Iterations):
 
     #Create numpy arrays of the time and errors to pass to L-S model
     Times = Data[['MJD']].as_matrix().ravel()
     Errors = Data[['Magerr']].as_matrix().ravel()
     Mag_orgs = Data[['Mag']].as_matrix().ravel()
 
+    #Define a colour palette, with the number of colours equal to the no. of iterations
+    cs = seaborn.color_palette(n_colors=Iterations)
+
     #The first element is always the original data set
     if number==0:
 
         #Build LSF model and generate file paths
         model_original = LombScargleFast().fit(Times, Mag_orgs, Errors)
-        file_path_org = file_loc + '/Light_Curve_org_' + Object + '.jpg'
-        file_path_org2 = file_loc + '/LSF_org_' + Object + '.jpg'
 
         #Build original model
         periods_orig, power_orig = model_original.periodogram_auto(nyquist_factor=0.1,oversampling=50)
@@ -59,31 +69,17 @@ def Iteration_function(Data, number, Output_figs, file_loc, Object):
         # find maximum of periodigram
         max_periods_orig = periods_orig[np.argmax(power_orig)]
 
+        #Crete a curve model for the best period
+        Curve_best_orig = np.linspace(Times.min(), Times.max(), 1000)
+
+        #Choose the best period using the optimizer
+        model_original.optimizer.period_range=(500, 2000)
+        best_periods_orig = model_original.best_period
+
         #Create Light Curve
         if Output_figs == 1:
-            fig, ax = plt.subplots()
-            ax.errorbar(Times, Mag_orgs, Errors, fmt='.k', color="r", ecolor='gray')
-            ax.set(xlabel='Time', ylabel='Magitude',
-                title= Object +' Light Curve Original')
-            ax.invert_yaxis();
-            fig.savefig(file_path_org, bbox_inches='tight')
-
-
-            #Plot the original LSF model
-            fig, ax = plt.subplots()
-            ax.plot(periods_orig, power_orig, color="r")
-            ax.set(xlim=(0, 2000),
-                    xlabel='Period (Days)',
-                    ylabel='Lomb-Scargle Power',
-                    title=Object + ' LS Power against period plot');
-            fig.savefig(file_path_org2, bbox_inches='tight')
-            plt.clf()
-        #Choose the best period, conditional on quality of model
-        if power_orig.max() < 0.1:
-            best_periods_orig = 0
-        else:
-            model_original.optimizer.period_range=(500, 2000)
-            best_periods_orig = model_original.best_period
+            plt.errorbar(Times, Mag_orgs, Errors, fmt='.k', color=cs[number], ecolor=cs[number])
+            plt.plot(Curve_best_orig, model_original.predict(Curve_best_orig, best_periods_orig),color='r',linewidth=2,zorder=Iterations)
 
         #Read best period out
         return [max_periods_orig, best_periods_orig]
@@ -96,32 +92,30 @@ def Iteration_function(Data, number, Output_figs, file_loc, Object):
     #Apply the LS mechanism to the data
     periods, power = model.periodogram_auto(nyquist_factor=0.1, oversampling=50)
     max_period = periods[np.argmax(power)]
-    file_path_Light_Curve = file_loc + '/Light_Curve_' + Object + '_' + str(number) + '.jpg'
-    file_path_LS_model = file_loc + '/LSF_' + Object + '_' + str(number) + '.jpg'
+    file_path_Light_Curve = file_loc + '/Light_Curve_' + Object + '.jpg'
 
-    #Plotting the Light Curve using seaborn style
-    if Output_figs == 1:
-        fig, ax = plt.subplots()
-        ax.errorbar(Times, Mags_mod, Errors, fmt='.k', color="r", ecolor='gray', label='Light Curve')
-        ax.set(xlabel='Time (MJD)',
-                ylabel='Magitude',
-                title=Object + ' Light Curve with Gaussian Noise iteration ' + str(number))
-        ax.invert_yaxis();
-        fig.savefig(file_path_Light_Curve, bbox_inches='tight')
+    #For plotting a sinusoid of the best period to the light curve
+    Curve_best = np.linspace(Times.min(), Times.max(), 1000)
 
-        #Plotting the LS model
-        fig, ax = plt.subplots()
-        ax.plot(periods, power, color="r", label='LSF')
-        ax.set(xlim=(0, 2000),
-                xlabel='Period (Days)',
-                ylabel='Lomb-Scargle Power',
-                title=Object + ' LS Power against period plot');
-        fig.savefig(file_path_LS_model, bbox_inches='tight')
-        plt.clf()
     #Calculating the best period of the model, condition on quality of LSF model
     model.optimizer.period_range=(500, 2000)
     best_period = model.best_period
 
+    #Plotting the Light Curve, with the iterations
+    if Output_figs == 1:
+        #Performing the actual plots
+        plt.plot(Curve_best, model.predict(Curve_best, best_period), color='g', linewidth=.25)
+
+        #Plot formatting
+        plt.xlabel('Time(MJD)')
+        plt.ylabel('Magitude')
+        plt.title(Object + ' Light Curve ' + str(number + 1) + ' iterations')
+        plt.gca().invert_yaxis()
+
+        #Saving the figure
+        plt.savefig(file_path_Light_Curve, bbox_inches='tight')
+
+    if max_period > 3000: max_period = 1000
     return [max_period,best_period]
 
     #End of Iteration_function
@@ -141,9 +135,12 @@ def calling_function(Data, Iterations, Outputs, Output_loc, Object):
 
     #Perform the iterations
     for j in range(0, Iterations):
-        New_Period = Iteration_function(Data, j, Output_n, Output_loc, Object)
+        New_Period = Iteration_function(Data, j, Output_n, Output_loc, Object, Iterations)
         Period_List.append(New_Period[0])
         Best_Period_List.append(New_Period[1])
+
+    #Clear the plot before the Histograms are plotted
+    plt.clf()
 
     #Call the LS_hist function in order to create the Histogram
     LS_hist(Best_Period_List, Output_loc, Object, Iterations, 'Best')
@@ -159,15 +156,18 @@ def LS_hist(Results, file_loc, Object, Iterations, string):
     ax.hist(Results, bins='auto')
     ax.set(xlabel = 'Period',
             ylabel = 'No. of Counts',
-            title = Object + string + " Histogram " + str(Iterations) + ' Iterations')
+            title = Object + ' ' + string + " Histogram " + str(Iterations) + ' Iterations')
 
     file_path_hist = file_loc + '\Hist ' + Object + ' ' + string + '.jpg'
     fig.savefig(file_path_hist, bbox_inches='tight')
     plt.clf()
     return(bins[np.argmax(n)])
     #End of LS_hist function
-"""
+
 def Graham_Candidate_Data_Grab(User):
+
+    #Designed to Grab the data URLs for the periodic candidates identified by
+    #Graham et al.
 
     #Define the path to the Graham Periodic Candidates
     if User == 'N':
@@ -240,12 +240,12 @@ def Graham_Candidate_Data_Grab(User):
     writing_file.close()
 
     #End of Graham_Candidate_Data_Grab function
-"""
 
 #Main function to be called
 def main():
 
-    User = 'C'
+    #Set the user of the program here
+    User = 'N'
 
     #Define the location of the saved path
     if(User == 'C'):
@@ -253,6 +253,7 @@ def main():
     if(User == 'N'):
         Data_location = 'C:/Users/User/Documents/University/Year 4/Project/Raw_Data_Inputs.txt'
 
+    #Calls the Data Grab function
     #Graham_Candidate_Data_Grab(User)
 
     #Read in the objects and URLs to be searched
@@ -263,10 +264,11 @@ def main():
     URLs = Raw_Data_input[['URL']].as_matrix()
 
     #Set the number of iterations here
-    No_of_Iterations = 2
+    No_of_Iterations = 1000
 
     #Whether to output Light curve and L-S figures or not (Y/N)
-    Output_figures = 'N'
+    Output_figures = 'Y'
+    if Output_figures == 'Y': plt.figure()
 
     #Output_location
     if (User == 'N'):
@@ -274,7 +276,9 @@ def main():
     elif (User == 'C'):
         Output_locations = 'C:/Users/Christopher/Documents/UNI/Year 4/Project/AGN-code/Iteration_figures_11-10-2016'
 
-    f = open("No. of periods.txt", 'w')
+    #Open up text file to write number of periods for each object
+    Period_data = open('No. of periods.txt','w')
+
     #Call the calling function for each object in the data
     for k in range(0, len(Raw_Data_input)):
         Object = Objects.item(k,0)
@@ -282,12 +286,16 @@ def main():
         URL_editted = URL.strip()
         Raw_Data = pd.read_csv(URL_editted, sep=',', header=0)
 
-        #Calling function runs program
-        pnumber = calling_function(Raw_Data, No_of_Iterations, Output_figures, Output_locations, Object)
+        if 'PG 1302' in Object:
+            #Calling function runs program
+            pnumber = calling_function(Raw_Data, No_of_Iterations, Output_figures, Output_locations, Object)
+            break
 
-        f.write(Object + ' ' + str(pnumber) + '\n')
+        #Write the period data into the text file
+        #Period_data.write(Object + ' ' + str(pnumber) + '\n')
 
-    f.close()
+    #Close the period data file
+    Period_data.close()
 
 #End of main function
 main()
